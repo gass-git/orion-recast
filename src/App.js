093a7fae20563844bb5b3653c6cc8e075fe22b83
Gabriel Salinas <75760163+gass-git@users.noreply.github.com>
2022-06-17 06:51:14 +0200

    UI improved

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { getNumberOfWords } from './functions/utilityFunctions'
 import { processInput } from './functions/processInput/processInput'
 import { appReducer, initialState } from './states'
 import AlertBox from './components/alertBox'
+import {Form, Row, Button, Container} from 'react-bootstrap'
 
 export const AppContext = createContext(null)
 
@@ -23,20 +24,43 @@ export default function App() {
 
   return (
     <AppContext.Provider value={{state, dispatch}}>
+      <div className='wrapper'>
       {
         output.success ? 
         null
         : 
         <AlertBox text={output.text} dispatch={dispatch} />
       }
-      <input name='inputString' value={input} onChange={handleChange}/>
-      <button onClick={() => handleClick()}>enter</button>
+
+      <Form.Group as={Row} className='mt-5 mb-4' constrolid='form-group-1'>
+        <Row>
+          <Form.Label>Intergalatic note: </Form.Label>
+        </Row>
+        <Row>
+          <Form.Control
+            type='string'
+            name='inputString'
+            value={input} 
+            onChange={handleChange}
+          />
+        </Row>
+        <Row>
+          <Button
+            variant='outline-primary'
+            className='mt-4 mb-3'
+            onClick={() => handleClick()}
+          >
+            Submit
+          </Button>
+        </Row>
+      </Form.Group >
       {
         output.success ? 
-        <p>{output.text}</p>
+        <div className='result'>{output.text}</div>
         : 
         null
       }
+      </div>
     </AppContext.Provider>
   )
 }
