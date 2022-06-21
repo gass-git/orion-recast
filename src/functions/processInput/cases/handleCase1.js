2296e0706c34866a907ae72f6357ca0e2996cfe6
Gabriel Salinas <g.szada@gmail.com>
2022-06-21 23:43:06 +0200

    small modification in handleCase1.js

--- src/functions/processInput/cases/handleCase1.js
+++ src/functions/processInput/cases/handleCase1.js
@@ -1,53 +1,53 @@
-export default function handleCase1(strings, dispatch, conversions){
-    let available = true
+export default function handleCase1(strings, dispatch, conversions) {
+  let available = true
 
-    // roman numeral should always be uppercase
-    let romanNumeral = strings[2].toUpperCase()
+  // roman numeral should always be uppercase
+  let romanNumeral = strings[2].toUpperCase()
 
-    // check if conversion name is been used
-    for(const romanNumeral in conversions){
-      if(strings[0] === conversions[romanNumeral]) available = false
-    }
+  // check if conversion name is been used
+  for (const romanNumeral in conversions) {
+    if (strings[0] === conversions[romanNumeral]) available = false
+  }
 
-    // if conversion name is available continue
-    if(available){
-      
-      // conditions
-      let C = [ 
-          romanNumeral === 'I',
-          romanNumeral === 'V',
-          romanNumeral === 'X',
-          romanNumeral === 'L',
-          romanNumeral === 'C',
-          romanNumeral === 'D',
-          romanNumeral === 'M'
-        ]
+  // if conversion name is available continue
+  if (available) {
 
-      if(C[0] || C[1] || C[2] || C[3] || C[4] || C[5] || C[6]){
-        dispatch({
-          type:'set name to roman numeral',
-          unitName: strings[0],
-          romanNumeral: romanNumeral
-        })
-        dispatch({
-          type:'update output',
-          success: true,
-          text:'updated successfully!'
-        })
-      }
-      else{
-        dispatch({
-          type:'update output',
-          success: false,
-          text: 'The input must ba a valid roman numeral. Make sure to use the following format: galactic-unit is roman-numeral. Example: flop is M'
-        })
-      }
-    }
-    else{
+    // conditions
+    let C = [
+      romanNumeral === 'I',
+      romanNumeral === 'V',
+      romanNumeral === 'X',
+      romanNumeral === 'L',
+      romanNumeral === 'C',
+      romanNumeral === 'D',
+      romanNumeral === 'M'
+    ]
+
+    if (C[0] || C[1] || C[2] || C[3] || C[4] || C[5] || C[6]) {
       dispatch({
-        type:'update output',
-        success: false,
-        text:'The conversion name is already been used'
+        type: 'set name to roman numeral',
+        unitName: strings[0],
+        romanNumeral: romanNumeral
+      })
+      dispatch({
+        type: 'update output',
+        success: true,
+        text: 'Updated successfully!'
       })
     }
+    else {
+      dispatch({
+        type: 'update output',
+        success: false,
+        text: 'The input must ba a valid roman numeral. Make sure to use the following format: galactic-unit is roman-numeral. Example: flop is M'
+      })
+    }
+  }
+  else {
+    dispatch({
+      type: 'update output',
+      success: false,
+      text: 'The conversion name is already been used'
+    })
+  }
 }
--- src/functions/processInput/cases/handleCase4.js
+++ src/functions/processInput/cases/handleCase4.js
@@ -1,66 +1,66 @@
-import {getArabicNumeral, convertToRoman} from '../../utilityFunctions'
-import {validation1, validation2, validation3, validation4, validation5} from '../../romanValidations'
+import { getArabicNumeral, convertToRoman } from '../../utilityFunctions'
+import { validation1, validation2, validation3, validation4, validation5 } from '../../romanValidations'
 import getTotal from '../../getTotal'
 
-export default function handleCase4(strings, dispatch, conversions, metalValues){
-    let galacticNumerals = strings.slice(4, strings.length - 2)
-    let romanNumerals = convertToRoman(galacticNumerals, conversions)
-    let metalName = strings[strings.length - 2]
-    let product = 0
-    let arabicNumerals = []
-    let [v1,v2,v3,v4,v5] = [null, null, null, null, null]
+export default function handleCase4(strings, dispatch, conversions, metalValues) {
+  let galacticNumerals = strings.slice(4, strings.length - 2)
+  let romanNumerals = convertToRoman(galacticNumerals, conversions)
+  let metalName = strings[strings.length - 2]
+  let product = 0
+  let arabicNumerals = []
+  let [v1, v2, v3, v4, v5] = [null, null, null, null, null]
 
-    // validate roman numeral logic
-    v1 = validation1(romanNumerals)
-    v2 = validation2(romanNumerals)
-    v3 = validation3(romanNumerals)
-    v4 = validation4(romanNumerals)
-    v5 = validation5(romanNumerals)
- 
-    // continue only if all validations have passed
-    if(v1 && v2 && v3 && v4 && v5){
+  // validate roman numeral logic
+  v1 = validation1(romanNumerals)
+  v2 = validation2(romanNumerals)
+  v3 = validation3(romanNumerals)
+  v4 = validation4(romanNumerals)
+  v5 = validation5(romanNumerals)
 
-      // convert roman numerals to arabic numerals
-      romanNumerals.forEach((symbol) => {
-        arabicNumerals.push(getArabicNumeral(symbol))
-      })
+  // continue only if all validations have passed
+  if (v1 && v2 && v3 && v4 && v5) {
 
-      // has the metal value been specified ?
-      if(metalValues[metalName] !== null){
-        
-        // multiply the result of the galactic numerals by the metal value
-        product = getTotal(arabicNumerals) * metalValues[metalName]
+    // convert roman numerals to arabic numerals
+    romanNumerals.forEach((symbol) => {
+      arabicNumerals.push(getArabicNumeral(symbol))
+    })
 
-        // if galactic numerals have not been defined
-        if(arabicNumerals.length === galacticNumerals.length){
-          dispatch({
-            type: 'update output',
-            success: true,
-            text: `${product} credits`
-          })
-        }
-        else{
-          dispatch({
-            type: 'update output',
-            success: false ,
-            text: 'Unknown intergalactic numeral/s'
-          })
-        }
-      }
-      else{
+    // has the metal value been specified ?
+    if (metalValues[metalName] !== null) {
+
+      // multiply the result of the galactic numerals by the metal value
+      product = getTotal(arabicNumerals) * metalValues[metalName]
+
+      // if galactic numerals have not been defined
+      if (arabicNumerals.length === galacticNumerals.length) {
         dispatch({
           type: 'update output',
-          success: false ,
-          text: `The value of ${metalName} hasn't been specified`
+          success: true,
+          text: `${product} credits`
         })
       }
-      
+      else {
+        dispatch({
+          type: 'update output',
+          success: false,
+          text: 'Unknown intergalactic numeral/s'
+        })
+      }
     }
-    else{
+    else {
       dispatch({
         type: 'update output',
         success: false,
-        text: 'The roman numerical logic is not valid'
+        text: `The value of ${metalName} hasn't been specified`
       })
     }
+
+  }
+  else {
+    dispatch({
+      type: 'update output',
+      success: false,
+      text: 'The roman numerical logic is not valid'
+    })
+  }
 }
